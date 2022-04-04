d143fbdfa92912f19f02e1f0dbcf9c121bf3cd38
Laura Flament <laura.flament6@gmail.com>
2022-04-04 22:08:33 +0200

    first commit

---
+++ front/js/index.js
@@ -0,0 +1,28 @@
+async function getProduct(){
+    return fetch("http://localhost:3000/api/products")
+    .then(function(result){
+        if(result.ok){
+            return result.json();
+        }
+    }).catch(function(error){
+        console.log(error);
+    });
+}
+
+async function main(){
+let listProduit = await getProduct();
+console.log(listProduit);
+let sectionItems = document.getElementById("items");
+for(let product of listProduit){
+    console.log(product.name);
+    sectionItems.innerHTML += `<a href="./product.html?id=${product._id}">`+
+    `<article>`+
+    `<img src="${product.imageUrl}" alt="${product.altTxt}">`+
+    `<h3 class="productName">${product.name}</h3>`+
+    `<p class="productDescription">${product.description}</p>`+
+    `</article>`+
+    `</a>`
+}
+}
+main();
+
