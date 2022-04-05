420c0d16bbec13dbc28ee530e068f179302cc2be
Laura Flament <laura.flament6@gmail.com>
2022-04-05 12:35:30 +0200

    Affichage du local storage sur la page panier

---
+++ front/js/cart.js
@@ -0,0 +1,62 @@
+async function getProduct(){
+    return fetch("http://localhost:3000/api/products")
+    .then(function(result){
+        if(result.ok){
+            return result.json();
+        }
+    }).catch(function(error){
+        console.log(error);
+    });
+}
+
+
+async function getCart(){
+let listProduit = await getProduct();
+console.log(listProduit);   
+
+let tableauProducts = JSON.parse(localStorage.getItem("product"));
+console.log(tableauProducts);
+
+let cartItems = document.getElementById("cart__items");
+
+for(let i=0; i< tableauProducts.length; i++){
+    let imageProduct;
+    let altTxtProduct;
+    console.log(tableauProducts[i].nameProduct);
+    for(let product of listProduit){
+        if (product._id == tableauProducts[i]._id){
+            imageProduct = product.imageUrl;
+            altTxtProduct = product.altTxt;
+            priceProduct = product.price;
+        }
+    }
+    cartItems.innerHTML += `<article class="cart__item" data-id="${tableauProducts[i]._id}" data-color="${tableauProducts[i].colorProduct}">` +
+            
+    `<div class="cart__item__img">` +
+    `<img src="${imageProduct}" alt="${altTxtProduct}" />` +
+    `</div>` +
+
+    `<div class="cart__item__content">` +
+    `<div class="cart__item__content__description">` +
+        `<h2>${tableauProducts[i].nameProduct}</h2>` +
+        `<p>${tableauProducts[i].colorProduct}</p>` +
+        `<p>${priceProduct}€</p>` +
+    `</div>` +
+
+    `<div class="cart__item__content__settings">` +
+        `<div class="cart__item__content__settings__quantity">` +
+        `<p>${tableauProducts[i].quantityProduct} </p>` +
+        `<input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${tableauProducts[i].quantityProduct}" />` +
+    `</div>` +
+        `<div class="cart__item__content__settings__delete">` +
+        `<p class="deleteItem">Supprimer</p>` +
+        `</div>` +
+    `</div>` +
+    `</div>` +
+    `</article> `;
+    
+    }
+    
+}
+
+getCart();
