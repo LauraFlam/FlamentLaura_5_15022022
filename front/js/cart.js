1d37c306691fd04b19ea8ca0f22da082cf03e92a
Laura Flament <laura.flament6@gmail.com>
2022-04-11 21:28:48 +0200

    Ajout des regex pour les champs du formulaire et affichage de message d'erreur

--- front/js/cart.js
+++ front/js/cart.js
@@ -100,6 +100,89 @@ for(let i=0; i< tableauProducts.length; i++){
         }
         }
     changeQuantity();
+
 }
 
 getCart();
+
+//Formulaire
+let form = document.querySelector(".cart__order__form");
+let emailRegEx = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-z]+[.]{1}[a-z]+$');
+let otherRegEx = new RegExp("^[a-zA-Z ,.'-]+$");
+let adressRegEx = new RegExp("^[0-9]{1,3}(?:(?:[,. ])[a-zA-Z]+)+$");
+
+form.email.addEventListener("change", function () {
+    validEmail(this);
+  });
+form.firstName.addEventListener("change", function () {
+    validFirstName(this);
+  });
+
+form.lastName.addEventListener("change", function () {
+    validLastName(this);
+  });
+
+form.address.addEventListener("change", function () {
+    validAddress(this);
+  });
+
+form.city.addEventListener("change", function () {
+    validCity(this);
+  });
+
+let validEmail = function(inputEmail){
+    let testEmail = emailRegEx.test(inputEmail.value);
+    let emailError = inputEmail.nextElementSibling;
+    if (testEmail == true){
+      return true;
+    } else {
+      emailError.innerHTML = "Veuillez renseigner correctement l'email.";
+      return false;
+    }
+}
+
+let validFirstName = function (inputFirstName) {
+    let firstNameError = inputFirstName.nextElementSibling;
+    let testFirstName = otherRegEx.test(inputFirstName.value);
+    if (testFirstName == true) {
+      return true;
+    } else {
+        firstNameError.innerHTML =
+        "Veuillez renseigner correctement le prénom.";
+      return false;
+    }
+};
+
+let validLastName = function (inputLastName) {
+    let lastNameError = inputLastName.nextElementSibling;
+    let testLastName = otherRegEx.test(inputLastName.value);
+    if (testLastName == true) {
+      return true;
+    } else {
+        lastNameError.innerHTML = "Veuillez renseigner correctement le nom de famille.";
+      return false;
+    }
+};
+
+let validAddress = function (inputAddress) {
+    let addressError = inputAddress.nextElementSibling;
+    let testAddress = adressRegEx.test(inputAddress.value);
+    if (testAddress == true) {
+      return true;
+    } else {
+        addressError.innerHTML = "Veuillez renseigner correctement l'adresse.";
+      return false;
+    }
+};
+
+let validCity = function (inputCity) {
+    let cityError = inputCity.nextElementSibling;
+    let testCity = otherRegEx.test(inputCity.value);
+    if (testCity == true) {
+      return true;
+    } else {
+        cityError.innerHTML = "Veuillez renseigner correctement la ville.";
+      return false;
+    }
+};
+
